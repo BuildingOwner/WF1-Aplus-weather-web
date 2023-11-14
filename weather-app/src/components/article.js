fba89c99c9d069292ca3ae38697af55b7710d9e6
BuildingOwner <wodhks0911@gmail.com>
2023-11-14 22:55:49 +0900

    통합중

--- weather-app/src/components/article.js
+++ weather-app/src/components/article.js
@@ -1,11 +1,15 @@
 import FavorateWeek from "./week/favorate-week";
 import Week from "./week/week";
+import NewsMain from "./news/NewsMain"
+import Main from "./Main/Main"
 
 const Article = ({location}) => {
   return (
     <>
       <FavorateWeek location={location} />
       <Week location={location} />
+      <NewsMain />
+      <Main />
     </>
   );
 };
