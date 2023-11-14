0dd7319c45592848e79818c9d45a5c2f2d0f3bbd
BuildingOwner <wodhks0911@gmail.com>
2023-11-14 23:57:15 +0900

    수정

--- weather-app/src/components/container.js
+++ weather-app/src/components/container.js
@@ -2,12 +2,6 @@ import Article from "./article";
 import DateTime from "./Main/DateTime"
 
 const Container = ({ location }) => {
-  let now = new Date();
-  let year = now.getFullYear();
-  let month = ("0" + (now.getMonth() + 1)).slice(-2);
-  let date = ("0" + now.getDate()).slice(-2);
-
-  let formattedDate = `${year}년 ${month}월 ${date}일`;
   
   return (
     <div style={{ minWidth: 1100, height: "100vh" }}>
