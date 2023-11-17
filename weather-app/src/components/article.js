c5987531d5ca2500f9ca38cd2badd3cbf926f213
jangjuya <109902601+jangjuya@users.noreply.github.com>
2023-11-17 12:06:46 +0900

    AirMain

--- weather-app/src/components/article.js
+++ weather-app/src/components/article.js
@@ -2,10 +2,7 @@ import FavorateWeek from "./week/favorate-week";
 import Week from "./week/week";
 import NewsMain from "./news/NewsMain";
 import Main from "./Main/MainPage";
-import Forecast from "./airPollution/Forecast";
-import Weather from "./airPollution/Weather";
-import Chart from "./airPollution/Chart";
-import Chart2 from "./airPollution/Chart2";
+import AirMain from "./airPollution/AirMain";
 
 const Article = ({ location }) => {
   return (
@@ -14,22 +11,7 @@ const Article = ({ location }) => {
       <Week location={location} />
       <NewsMain />
       <Main />
-      <div>
-        <h4>Weather Forecast</h4>
-        <Forecast />
-      </div>
-      <div className="weather-container">
-        <h4>내 지역 상세</h4>
-        <Weather />
-      </div>
-      <div>
-        <h4>시간대별 관측 및 예보</h4>
-        <Chart />
-      </div>
-      <div>
-        <h4>주간예보</h4>
-        <Chart2 />
-      </div>
+      <AirMain />
     </>
   );
 };
