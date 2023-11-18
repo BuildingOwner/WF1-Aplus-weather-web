1dfe954f1e6a61af7a92539f9e1b990dc54e3878
BuildingOwner <wodhks0911@gmail.com>
2023-11-18 18:54:21 +0900

    datetime 수정

--- weather-app/src/components/Main/DateTime.js
+++ weather-app/src/components/Main/DateTime.js
@@ -3,6 +3,14 @@ import "../../css/header/DateTime.css";
 function DateTime() {
   const [now, setNow] = useState(new Date());
 
+  const year = now.getFullYear(); // 년도를 가져옵니다.
+  const month = now.getMonth() + 1; // 월을 가져옵니다. (0~11로 표현되므로 +1을 해줍니다)
+  const day = now.getDate(); // 일을 가져옵니다.
+
+  // 요일을 가져옵니다. (0~6으로 표현되므로 해당 요일에 맞는 한글을 배열에서 찾습니다)
+  const weekdays = ["일", "월", "화", "수", "목", "금", "토"];
+  const weekday = weekdays[now.getDay()];
+
   useEffect(() => {
     const timer = setInterval(() => {
       setNow(new Date());
@@ -13,13 +21,12 @@ function DateTime() {
     };
   }, []);
 
-  const date = now.toLocaleDateString();
   const time = now.toLocaleTimeString();
 
   return (
     <div className="date-time">
       <span className="date-time-time">{time}</span>
-      <span>{date}</span>
+      <span className="date-time-date">{year}년 {month}월 {day}일 {weekday}요일</span>
     </div>
   );
 }
