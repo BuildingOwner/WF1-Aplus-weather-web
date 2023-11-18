d75df0ab58211334af47ae2bb8c2371df8ef0c5b
aodlf452 <aodlf452@gmail.com>
2023-11-18 14:17:19 +0900

    css수정

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,14 +1,14 @@
-import './css/App.css'
-import './css/reset.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import useCurrentLocation from './hooks/useCurrentLocation'
-import Expention from './components/exponsion/expension'
-import Sidebar from './components/Sidebar/Sidebar';
-import Main from './components/Main/MainPage';
-import WeekPage from './components/week/week-page';
-import AirMain from './components/airPollution/AirMain';
-import NewsMain from './components/news/NewsMain';
-
+import "./css/App.css";
+import "./css/reset.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import useCurrentLocation from "./hooks/useCurrentLocation";
+import Expention from "./components/exponsion/expension";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/MainPage";
+import WeekPage from "./components/week/week-page";
+import AirMain from "./components/airPollution/AirMain";
+import NewsMain from "./components/news/NewsMain";
+import DateTime from "./components/Main/DateTime";
 function App() {
   const { loaded, address, error } = useCurrentLocation();
 
@@ -18,17 +18,23 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-				<Sidebar />
-				<Routes>
-					<Route path="/" element={<Main />}></Route>
-					<Route path="/week/*" element={<WeekPage location={address}/>}></Route>
-					<Route path="/airPollution/*" element={<AirMain />}></Route>
-					<Route path="/news/*" element={<NewsMain />}></Route>
-					{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
-					{/* <Route path="*" element={<NotFound />}></Route> */}
-				</Routes>
+        <Sidebar />
+        <div className="center-content">
+          <DateTime />
+          <Routes>
+            <Route path="/" element={<Main />}></Route>
+            <Route
+              path="/week/*"
+              element={<WeekPage location={address} />}
+            ></Route>
+            <Route path="/airPollution/*" element={<AirMain />}></Route>
+            <Route path="/news/*" element={<NewsMain />}></Route>
+            {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+            {/* <Route path="*" element={<NotFound />}></Route> */}
+          </Routes>
+        </div>
         <Expention location={address} />
-			</BrowserRouter>
+      </BrowserRouter>
     </div>
   );
 }
--- weather-app/src/components/Main/DateTime.js
+++ weather-app/src/components/Main/DateTime.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import "../../css/header/DateTime.css";
 function DateTime() {
   const [now, setNow] = useState(new Date());
 
@@ -16,7 +16,7 @@ function DateTime() {
   const date = now.toLocaleDateString();
   const time = now.toLocaleTimeString();
 
-  console.log(date)
+  console.log(date);
   return (
     <div className="date-time">
       <p>{date}</p>
